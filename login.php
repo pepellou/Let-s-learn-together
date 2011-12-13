608c7fdfce89791671e595be1f2d72bf45c4744f
Pepe Doval <pepellou@gmail.com>
2011-12-13 10:48:24 +0100

    first try to login page

---
+++ login.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>Let's learn together</title>
+	<link rel=stylesheet type=text/css href=css/style.css />
+</head>
+<body>
+	<header class=main>
+		<img src=img/cabecera.png />
+	</header>
+	<article>
+		<p>
+			<h1>Quiero aprender...</h1>
+			Selecciona el usuario con el que deseas entrar:<br/>
+			<ul>
+				<li><a href=login.php?user=eanovoa>eanovoa</a></li>
+				<li><a href=login.php?user=mininacheshire>mininacheshire</a></li>
+				<li><a href=login.php?user=pepellou>pepellou</a></li>
+			</ul>
+		</p>
+	</article>
+</body>
+</html>
